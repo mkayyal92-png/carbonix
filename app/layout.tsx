7640fac7bedaa9ba0f4dbd5b8a4d708c038d8e60
Lumira House <237113801+mkayyal92-png@users.noreply.github.com>
2026-06-05 04:07:57 +0300

    Change body background color and text color

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="ar">
-      <body style={{ margin: 0, background: "#000" }}>
+      <body style={{ margin: 0, background: "#0A0A0A", color: "white" }}>
         {children}
       </body>
     </html>
